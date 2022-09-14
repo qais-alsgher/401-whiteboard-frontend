09960f95663f4ac38460826b728d345dd2fdc205
qais-alsgher <qaisalsgher@gmail.com>
2022-09-14 23:59:16 +0300

    giv margin to ceard

--- src/components/CaedPost.js
+++ src/components/CaedPost.js
@@ -6,7 +6,7 @@ function CaedPost(props) {
     const { user, isAuthenticated } = useAuth0();
     return (
         <div>
-            <Card style={{ width: '100%' }}>
+            <Card style={{ width: '100%', margin: "20PX" }}>
                 <Card.Text>{props.post.postAouthr}</Card.Text>
                 <Card.Img variant="top" src={props.post.postImge} />
                 <Card.Body>
